639002e08bdcc1ce829c44e04a198c7ba58cbd05
Oliver Isenrich <IsenrichO@gmail.com>
2017-02-25 18:49:08 -0800

    feat (React): Constrain figcaption width using Promises

--- src/ImgBlock.jsx
+++ src/ImgBlock.jsx
@@ -2,14 +2,33 @@
 import React from 'react';
 
 
-const ImgBlock = ({ url, title }) => (
-	<div className="img-block">
-		<img
-			src={ url }
-			alt='image' />
-		<p>{ title }</p>
-	</div>
-);
+// Promisify image loading before `then`ing style manipulation:
+const gg = (index, url, title) => {
+  let img = new Image(),
+      adjustedWidth;
+  return new Promise(function(resolve, reject) {
+    img.src = url;
+    img.onload = function() {
+      resolve((this.width / this.height) * (window.innerHeight / 10));
+    }
+  })
+  .then(val => {
+    document.querySelector(`.block-${index}`).style.maxWidth = `${val}px`;
+  });
+};
 
+
+const ImgBlock = ({ index, url, title }) => {
+  const titleWidth = gg(index, url, title);
+
+  return (
+  	<figure className={ `img-block block-${index}` }>
+  		<img
+  			src={ url }
+  			alt='Flickr Image' />
+  		<figcaption>{ title }</figcaption>
+  	</figure>
+  );
+};
 
 export default ImgBlock;
